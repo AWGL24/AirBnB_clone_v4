4d9004eadb739317e2f0cca92f51065b7f62615c
Drew-24 <andrewwil1@hotmail.com>
2022-05-29 23:23:20 -0400

    Fetch places

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,16 @@
 $(function () {
-	$('.amenities UL LI INPUT').css('margin-right', '10px');
-	let id_dictionary = {};
-	$(':checkbox').change(function () {
-		if (this.checked) {
-			id_dictionary[$(this).data('id')] = $(this).data('name');
-		} else if (this.not.checked) {
-			delete id_dictionary[$(this).data('id')];
-		}
-		let list = [];
-		for (let k in id_dictionary) {
-			list.push(id_dictionary[k]);
-		}
-		$('.amenities h4').text(list.join(', '));
-	});
+  $('.amenities UL LI INPUT').css('margin-right', '10px');
+  const id_dictionary = {};
+  $(':checkbox').change(function () {
+    if (this.checked) {
+      id_dictionary[$(this).data('id')] = $(this).data('name');
+    } else if (this.not.checked) {
+      delete id_dictionary[$(this).data('id')];
+    }
+    const list = [];
+    for (const k in id_dictionary) {
+      list.push(id_dictionary[k]);
+    }
+    $('.amenities h4').text(list.join(', '));
+  });
 });
