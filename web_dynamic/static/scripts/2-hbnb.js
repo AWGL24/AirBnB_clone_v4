d18a83a53da7b2aebcfa7df333e864787050d529
Drew-24 <andrewwil1@hotmail.com>
2022-05-29 21:37:26 -0400

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$(function () {
+	$('.amenities UL LI INPUT').css('margin-right', '10px');
+	let id_dictionary = {};
+	$(':checkbox').change(function () {
+		if (this.checked) {
+			id_dictionary[$(this).data('id')] = $(this).data('name');
+		} else if (this.not.checked) {
+			delete id_dictionary[$(this).data('id')];
+		}
+		let list = [];
+		for (let k in id_dictionary) {
+			list.push(id_dictionary[k]);
+		}
+		$('.amenities h4').text(list.join(', '));
+	});
+	$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+		if (data.status == 'OK') {
+			$('DIV#api_status').addClass('available');
+		} else {
+			$('DIV#api_status').toggleClass('available');
+		}
+	});
+});
